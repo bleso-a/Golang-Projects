fc70beae5a13023830e1088bf0b012f3906474f8
Blessing Adetoye Adesiji <38926059+bleso-a@users.noreply.github.com>
2020-04-05 18:10:40 +0100

    Add files via upload
    Project - Book Publisher tracker

--- main.go
+++ main.go
@@ -3,18 +3,30 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("    `.-::::::-.`    ")
-  fmt.Println(".:-::::::::::::::-:.")
-  fmt.Println("`_:::    ::    :::_`")
-  fmt.Println(".:( ^   :: ^   ):.")
-  fmt.Println("`:::   (..)   :::.")
-  fmt.Println("`:::::::UU:::::::`")
-  fmt.Println(".::::::::::::::::.")
-  fmt.Println("O::::::::::::::::O")
-  fmt.Println("-::::::::::::::::-")
-  fmt.Println("`::::::::::::::::`")
-  fmt.Println(" .::::::::::::::. ")
-  fmt.Println("   oO:::::::Oo    ")
+  var publisher, writer, artist, title string
+  var year, pageNumber int32
+  var grade float32
   
-
+  title = "Mr. GoToSleep"
+  writer = "Tracey Hatchet"
+  artist = "Jewel Tampson"
+  publisher = "DizzyBooks Publishing Inc"
   
+  year = 1997
+  pageNumber = 14
+  grade = 6.5
+  
+  
+  
+  fmt.Println(title, "written by", writer, "drawn by", artist, "in", year, "with total page number", pageNumber, "and grade", grade, ", published by", publisher)
+  
+  title = "Epic Vol. 1"
+  writer = "Ryan N. Shawn"
+  artist = "Phoebe Paperclips"
+  year = 2013
+  pageNumber = 160
+  grade = 9.0
+  
+    fmt.Println(title, "written by", writer, "drawn by", artist, "in", year, "with total page number", pageNumber, "and grade", grade, ", published by", publisher)
+ 
+}
